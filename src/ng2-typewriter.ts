e69c4fefa73c58b3034de09e189d5fc054d50da9
Gale Yao <yaogang1987613@163.com>
2017-01-23 13:28:49 +0800

    Export typewriter content and service.

--- src/ng2-typewriter.ts
+++ src/ng2-typewriter.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { TypewriterDirective } from "./typewriter.directive";
-import { TypewriterService } from "./typewriter.service";
-import { TypewriterContent } from "./typewriter.content";
 import { CursorComponent } from "./cursor/cursor.component";
 import { TypewriterComponent } from "./typewriter.component";
 
+export * from "./typewriter.content";
+export * from "./typewriter.service";
+
 @NgModule({
     declarations: [
-        TypewriterContent,
         TypewriterDirective,
-        TypewriterService,
         TypewriterComponent,
         CursorComponent
     ],
     exports: [
-        TypewriterContent,
         TypewriterDirective,
-        TypewriterService,
         TypewriterComponent,
         CursorComponent
     ]

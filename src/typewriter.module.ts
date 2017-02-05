7ac09b097e1f74d6b2a0ceb7c5f28c0a5aea046b
Gale Yao <yaogang1987613@163.com>
2017-02-05 13:40:05 +0800

    Move extra exports into index.ts

--- src/typewriter.module.ts
+++ src/typewriter.module.ts
@@ -3,22 +3,17 @@ import { TypewriterDirective } from "./typewriter.directive";
 import { CursorComponent } from "./cursor/cursor.component";
 import { TypewriterComponent } from "./typewriter.component";
 
-export * from "./typewriter.content";
-export * from "./typewriter.service";
-
 @NgModule({
     declarations: [
         TypewriterDirective,
         TypewriterComponent,
-        CursorComponent
+        CursorComponent,
     ],
     exports: [
         TypewriterDirective,
         TypewriterComponent,
-        CursorComponent
+        CursorComponent,
     ]
 })
 
-export class TypewriterModule {
-
-}
+export class TypewriterModule {}
